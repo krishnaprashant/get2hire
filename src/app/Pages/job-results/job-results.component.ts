eddd38af1709dad195f853f4d49f36d7d85ec78f
Prashant <prashantkrishna5@gmail.com>
2019-07-22 17:41:44 +0530

    uploaded to server with autocomplete functionality

---
+++ src/app/Pages/job-results/job-results.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "app-job-results",
+  templateUrl: "./job-results.component.html",
+  styleUrls: ["./job-results.component.css"]
+})
+export class JobResultsComponent implements OnInit {
+  keyword: string;
+  skill: string;
+  location: string;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.keyword = this.route.snapshot.paramMap.get("keyword");
+    this.location = this.route.snapshot.paramMap.get("location");
+    this.skill = this.route.snapshot.paramMap.get("skill");
+  }
+}
